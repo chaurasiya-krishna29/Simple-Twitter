a5512ee531993a978b3b01d4a73bbbea0d1825a8
Krishna Chaurasiya <krishnachaurasiya2907@gmail.com>
2024-09-17 23:42:12 +0530

    Twitter Clone

---
+++ tweet/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render,HttpResponse
+from .models import *
+from .forms import *
+from django.shortcuts import get_object_or_404,redirect,HttpResponse
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def home(request):
+    return HttpResponse("Hello Buddyss")
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request,'tweet_list.html',{'tweets':tweets})
+
+@login_required
+def tweet_create(request):
+    if request.method=="POST":
+        form = TweetForm(request.POST,request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)#commit is false because we dont want to save in database we are temporary saving it
+            tweet.user = request.user #we want to add user also so we will call user from request, because from the form we will not get user we are calling from models
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form =TweetForm()
+    return render(request,'tweet_form.html',{'form':form})
+
+
+@login_required
+def tweet_edit(request,tweet_id):#tweet_id is the id of the tweets created ,pk stands for primary key,Tweet is the model created in the model file
+    tweet = get_object_or_404(Tweet, pk=tweet_id,user=request.user)#user=request.user is used to check that the owner of the user who is login can only edit the tweets
+    if request.method == "POST":
+        form = TweetForm(request.POST,request.FILES,instance=tweet)#instance=tweet is used to get the tweet that is to be edited
+        if form.is_valid():
+            tweet=form.save(commit=False)
+            tweet.user=request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else :
+        form = TweetForm(instance=tweet)
+    return render(request,'tweet_form.html',{'form':form})
+
+@login_required
+def tweet_delete(request,tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id,user = request.user)
+    if request.method == "POST":
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(request,'tweet_confirm_delete.html',{'tweet':tweet})
